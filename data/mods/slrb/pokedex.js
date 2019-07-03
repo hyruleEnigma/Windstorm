575d69cfa6ae88e1c1c47c5e3f3f460fa229f788
fart <tyler.m.sciara@gmail.com>
2019-07-03 17:17:15 -0400

    Update pokedex.js

--- data/mods/slrb/pokedex.js
+++ data/mods/slrb/pokedex.js
@@ -9,6 +9,11 @@ let BattlePokedex = {
 		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100}, // the base stats for the pokemon
 	},
 	*/
+	// A
+	mew: {
+		inherit: true,
+		types: ['Psychic', 'Fairy'],
+	},
 	// CJer
 	porygon: {
 		inherit: true,
