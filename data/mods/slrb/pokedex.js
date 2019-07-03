fb19a8294082b459773f31ca4e169a68dbd49d4e
fart <tyler.m.sciara@gmail.com>
2019-07-03 15:58:24 -0400

    Update pokedex.js

--- data/mods/slrb/pokedex.js
+++ data/mods/slrb/pokedex.js
@@ -9,23 +9,28 @@ let BattlePokedex = {
 		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100}, // the base stats for the pokemon
 	},
 	*/
-
-	// fart
-	kartana: {
-		inherit: true,
-		types: ['Fairy', 'Steel'],
-	},
 	// CJer
 	porygon: {
 		inherit: true,
 		baseStats: {hp: 105, atk: 100, def: 110, spa: 125, spd: 115, spe: 80},
 	},	
-
+	// fart
+	kartana: {
+		inherit: true,
+		types: ['Fairy', 'Steel'],
+	},
 	// MdPikachu
 	pikachu: {
 		inherit: true,
 		baseStats: {hp: 70, atk: 110, def: 80, spa: 100, spd: 100, spe: 180},
-	},	
+	},
+	// Moxie Latios
+	latiosmega: {
+		inherit: true,
+		abilities: {0: 'Moxie'},
+		types: ['Dragon', 'Ground'],
+		baseStats: {hp: 80, atk: 160, def: 100, spa: 130, spd: 120, spe: 110},
+	},
 };
 
 exports.BattlePokedex = BattlePokedex;
