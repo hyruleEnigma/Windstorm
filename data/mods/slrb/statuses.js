3d97701cc028d10fa79af6978ab2d1b582d9ef51
fart <tyler.m.sciara@gmail.com>
2019-07-04 17:30:03 -0400

    Update statuses.js

--- data/mods/slrb/statuses.js
+++ data/mods/slrb/statuses.js
@@ -75,7 +75,18 @@ let BattleStatuses = {
 			return ["Ghost", "Grass"];
 		},
 	},
-	
+	bigboyteddy: {
+		noCopy: true, 
+		onStart: function () {
+			this.add(`c| Big Boy Teddy|Did someone say muffin?`);
+		},
+		onSwitchOut: function () {
+			this.add(`c| Big Boy Teddy|gtg, luv u all! except u Moxie Latios >:(`);
+		},
+		onFaint: function () {
+			this.add(`c| Big Boy Teddy|where da muffin at? :(`);
+		},
+	},
 	brucewee: {
 		noCopy: true, 
 		onStart: function () {
@@ -87,7 +98,19 @@ let BattleStatuses = {
 		onFaint: function () {
 			this.add(`c| BruceWee|weird flex but ok`);
 		},
-	},	
+	},
+	cjthegold: {
+		noCopy: true,
+		onStart(target, source) {
+			this.add(`c| CJtheGold ☯|Hello there, General CJ`);
+		},
+		onSwitchOut() {
+			this.add(`c| CJtheGold ☯|Behold the power of gold when it returns...`);
+		},
+		onFaint() {
+			this.add(`c| CJtheGold ☯|Cya nerds <3`);
+		},
+	},
 	fart: {
 		noCopy: true,
 		onStart(target, source) {
@@ -101,19 +124,110 @@ let BattleStatuses = {
 			this.add(`c|#fart|the things I do for love...`);
 		},
 	},
+	hostjoe: {
+		noCopy: true, 
+		onStart() {
+			this.add(`c|@Host Joe|Im about to crack open a cold one, Boy!`);
+		},
+		onSwitchOut() {
+			this.add(`c|@Host Joe|I'm trying, Jennifer!`);
+		},
+		onFaint() {
+			this.add(`c|@Host Joe|Cavs in 7`);
+		},
+	},
+	hyruleenigma: {
+		noCopy: true, 
+		onStart() {
+			this.add(`c|%hyruleEnigma|Ahoy, Lobby!`);
+		},
+		onSwitchOut() {
+			this.add(`c|%hyruleEnigma|If only I were Dunsparce...`);
+		},
+		onFaint() {
+			this.add(`c|%hyruleEnigma|I should've buffed Sword Beam.`);
+		},
+	},
+	krookies: {
+		noCopy: true, 
+		onStart() {
+			this.add(`c| Krookies|I love all of you!`);
+		},
+		onSwitchOut() {
+			this.add(`c| Krookies|Im gonna go eat some potatoes brb`);
+		},
+		onFaint() {
+			this.add(`c| Krookies|Dying is bad for your health :c`);
+		},
+	},
 	"lumiq": {
 		noCopy: true, 
-		onStart: function () {
+		onStart() {
 			this.add(`c|%Lumi Q|Fly like a butterfly; sting like me C;<`);
 		},
-		onSwitchOut: function () {
+		onSwitchOut() {
 			this.add(`c|%Lumi Q|Tag out!`);
 		},
-		onFaint: function () {
+		onFaint() {
 			this.add(`c|%Lumi Q|ded`);
 		},
 	},
-	
+	marukomuru: {
+		noCopy: true, 
+		onStart(target) {
+			this.add(`c| Marukomuru|Welcome to the Lobby :DDDD`);
+			if (target.illusion) return;
+			this.boost({atk: 2}, target);
+		},
+		onSwitchOut() {
+			this.add(`c| Marukomuru|I was supposed to have a quote here for if I get switched out, but wasn't aware of that being a thing in the first place; this placeholder text will have to do.`);
+		},
+		onFaint() {
+			this.add(`c| Marukomuru|So you don't have a favorite pony? :C`);
+		},
+	},
+	mdpikachu: {
+		noCopy: true, 
+		onStart() {
+			this.add(`c| MdPikachu|Will I be a voice if I report enough people?`);
+		},
+		onSwitchOut() {
+			this.add(`c| MdPikachu|Nope, nope, I want none of this!`);
+		},
+		onFaint() {
+			this.add(`c| MdPikachu|This is fair and balanced!`);
+		},
+	},
+	mobilegreennamed: {
+		noCopy: true, 
+		onStart() {
+			this.add(`c| MobileGreenNamed|Time for my daily ran-oh wait! We're battling?`);
+		},
+		onSwitchOut() {
+			this.add(`c| MobileGreenNamed|/me jumps out window`);
+		},
+		onFaint() {
+			this.add(`c| MobileGreenNamed|X_X`);
+		},
+	},
+	moxielatios: {
+		noCopy: true,
+		onStart(target, source) {
+			this.add(`c|+Moxie ♥ Latios|Latios does get moxie idiot.`);
+		},
+		onSwitchOut() {
+			this.add(`c|+Moxie ♥ Latios|I need to find where I asked for your opinion, one second.`);
+		},
+		onFaint() {
+			this.add(`c|+Moxie ♥ Latios|My Lobby needs me.`);
+		},
+	},
+	megalatios: {
+		noCopy: true,
+		onStart(target, source) {
+			this.add('-start', source, 'typechange', `Dragon/Ground`);
+		},
+	},
 	mustard: {
 		noCopy: true, 
 		onStart: function () {
@@ -124,33 +238,6 @@ let BattleStatuses = {
 		},
 		onFaint: function () {
 			this.add(`c|@mustard|Unluckeee`);
-		},
-	},
-
-
-	mdpikachu: {
-		noCopy: true, 
-		onStart: function () {
-			this.add(`c| MdPikachu|Will I be a voice if I report enough people?`);
-		},
-		onSwitchOut: function () {
-			this.add(`c| MdPikachu|Nope, nope, I want none of this!`);
-		},
-		onFaint: function () {
-			this.add(`c| MdPikachu|This is fair and balanced!`);
-		},
-	},
-	
-	hyruleenigma: {
-		noCopy: true, 
-		onStart: function () {
-			this.add(`c|%hyruleEnigma|Ahoy, Lobby!`);
-		},
-		onSwitchOut: function () {
-			this.add(`c|%hyruleEnigma|If only I were Dunsparce...`);
-		},
-		onFaint: function () {
-			this.add(`c|%hyruleEnigma|I should've buffed Sword Beam.`);
 		},
 	},
    rYGLY: {
@@ -177,36 +264,6 @@ let BattleStatuses = {
            this.add(`c|%SacredLatias|Them spoopnoodles finally got the best of me...`);
        },
    },
-	mobilegreennamed: {
-		noCopy: true, 
-		onStart: function () {
-			this.add(`c| MobileGreenNamed|Time for my daily ran-oh wait! We're battling?`);
-		},
-		onSwitchOut: function () {
-			this.add(`c| MobileGreenNamed|/me jumps out window`);
-		},
-		onFaint: function () {
-			this.add(`c| MobileGreenNamed|X_X`);
-		},
-	},
-	moxielatios: {
-		noCopy: true,
-		onStart(target, source) {
-			this.add(`c|+Moxie ♥ Latios|Latios does get moxie idiot.`);
-		},
-		onSwitchOut() {
-			this.add(`c|+Moxie ♥ Latios|I need to find where I asked for your opinion, one second.`);
-		},
-		onFaint() {
-			this.add(`c|+Moxie ♥ Latios|My Lobby needs me.`);
-		},
-	},
-	megalatios: {
-		noCopy: true,
-		onStart(target, source) {
-			this.add('-start', source, 'typechange', `Dragon/Ground`);
-		},
-	},
 	tenshi: {
 		noCopy: true, 
 		onStart() {
@@ -217,19 +274,6 @@ let BattleStatuses = {
 		},
 		onFaint() {
 			this.add(`c|@Tenshi|Growl Bork Bork :C`);
-		},
-	},
-	
-	krookies: {
-		noCopy: true, 
-		onStart: function () {
-			this.add(`c| Krookies|I love all of you!`);
-		},
-		onSwitchOut: function () {
-			this.add(`c| Krookies|Im gonna go eat some potatoes brb`);
-		},
-		onFaint: function () {
-			this.add(`c| Krookies|Dying is bad for your health :c`);
 		},
 	},
 	pinkdragontamer: {
@@ -244,18 +288,6 @@ let BattleStatuses = {
 			this.add(`c|%pinkdragontamer|!faq you`);
 		},
 	},
-	hostjoe: {
-		noCopy: true, 
-		onStart: function () {
-			this.add(`c|@Host Joe|Im about to crack open a cold one, Boy!`);
-		},
-		onSwitchOut: function () {
-			this.add(`c|@Host Joe|I'm trying, Jennifer!`);
-		},
-		onFaint: function () {
-			this.add(`c|@Host Joe|Cavs in 7`);
-		},
-	},
 	majesticlucario: {
 		noCopy: true, 
 		onStart: function () {
@@ -266,30 +298,6 @@ let BattleStatuses = {
 		},
 		onFaint: function () {
 			//this.add(`c|@Host Joe|Cavs in 7`);
-		},
-	},
-	bigboyteddy: {
-		noCopy: true, 
-		onStart: function () {
-			this.add(`c| Big Boy Teddy|Did someone say muffin?`);
-		},
-		onSwitchOut: function () {
-			this.add(`c| Big Boy Teddy|gtg, luv u all! except u Moxie Latios >:(`);
-		},
-		onFaint: function () {
-			this.add(`c| Big Boy Teddy|where da muffin at? :(`);
-		},
-	},
-	cjthegold: {
-		noCopy: true,
-		onStart(target, source) {
-			this.add(`c| CJtheGold ☯|Hello there, General CJ`);
-		},
-		onSwitchOut() {
-			this.add(`c| CJtheGold ☯|Behold the power of gold when it returns...`);
-		},
-		onFaint() {
-			this.add(`c| CJtheGold ☯|Cya nerds <3`);
 		},
 	},
 	flare: {
