e419fca52b6c5bbd48abd8c95aee07f937eee24e
fart <tyler.m.sciara@gmail.com>
2019-07-04 19:01:25 -0400

    Update moves.js

--- data/mods/slrb/moves.js
+++ data/mods/slrb/moves.js
@@ -96,6 +96,36 @@ let BattleMovedex = {
 		target: "self",
 		type: "Normal",
 	},
+	// A Phantom
+	"forestfire": {
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "Burns and adds Ghost to the target.",
+		shortDesc: "Burns and adds Ghost to the target.",
+		id: "forestfire",
+		name: "Forest Fire",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, reflectable: 1, mirror: 1, mystery: 1},
+		onHit(target) {
+			if (target.hasType('Ghost')) return false;
+			if (!target.addType('Ghost')) return false;
+			this.add('-start', target, 'typeadd', 'Ghost', '[from] move: Trick-or-Treat');
+
+			if (target.side.active.length === 2 && target.position === 1) {
+				// Curse Glitch
+				const action = this.willMove(target);
+				if (action && action.move.id === 'curse') {
+					action.targetLoc = -1;
+				}
+			}
+		},
+		status: 'brn',
+		secondary: null,
+		target: "normal",
+		type: "Ghost",
+	},
 	// ArchasTL
 	"nerdslandering": {
 		accuracy: 100,
