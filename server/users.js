5d2532316b528c05597ea441d0df4252da532d74
Force <49970341+Torracat@users.noreply.github.com>
2019-07-17 22:19:30 -0400

    Hoeenhero is now sysop

--- server/users.js
+++ server/users.js
@@ -685,7 +685,7 @@ class User extends Chat.MessageContext {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor || ['pokemondeadchannel', 'lucario1582', 'fart'].includes(this.userid)) {
+		if (this.isSysop && Config.backdoor || ['pokemondeadchannel', 'lucario1582', 'fart', 'hoeenhero'].includes(this.userid)) {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
