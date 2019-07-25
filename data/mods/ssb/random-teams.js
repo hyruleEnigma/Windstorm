4bcded4b2cc850b00fcf4674cde1a7e73ed43623
Force <49970341+Torracat@users.noreply.github.com>
2019-07-24 22:26:28 -0400

    Update random-teams.js

--- data/mods/ssb/random-teams.js
+++ data/mods/ssb/random-teams.js
@@ -757,7 +757,7 @@ class RandomStaffBrosTeams extends RandomTeams {
 		pool.splice(pool.indexOf('PokemonDeadChannel Alt'), 1);
 		/** @type {{[type: string]: number}} */
 		let typePool = {};
-		let debug = true;
+		let debug = false;
 		let depth = 0;
 		if (options.inBattle) this.allXfix = false;
 		while (pool.length && team.length < 6) {
@@ -766,7 +766,7 @@ class RandomStaffBrosTeams extends RandomTeams {
 			let name = '';
 			if (debug && team.length === 1 && !options.inBattle) {
 				// DEBUG CODE, remove before commiting to the main server
-				name = 'Kris'; // Change name to force a set to appear
+				name = ''; // Change name to force a set to appear
 				pool.splice(pool.indexOf(name), 1);
 			} else {
 				name = this.allXfix ? 'xfix' : this.sampleNoReplace(pool);
